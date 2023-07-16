03a8a63fee0292f1e9f86a09f45beecd23e30b8d
rkbs <renukaraju.kbs@dxc.com>
2023-07-16 12:42:13 +0530

    Checking 1

---
+++ common-pipeline.groovy
@@ -0,0 +1,29 @@
+def test1(){
+    git clone https://github.com/Rajugithub1989/java-hello-world-webapp-1.git
+}
+def test2(){
+    git clone https://github.com/Rajugithub1989/java-hello-world-webapp-2.git
+}
+//def test3(){
+//    git clone https://github.com/Rajugithub1989/java-hello-world-webapp-3.git
+//}
+return this
+
+
+
+
+
+
+
+
+
+
+// { 
+//     node() {
+//        git clone https://github.com/Rajugithub1989/java-hello-world-webapp-1.git
+//        git clone https://github.com/Rajugithub1989/java-hello-world-webapp-2.git
+//        git clone https://github.com/Rajugithub1989/java-hello-world-webapp-3.git            
+
+//        // Whatever you do with your 3 repos...
+//     }
+// }
---
+++ second.groovy
@@ -0,0 +1,13 @@
+import groovy.transform.Field
+@Field private First = null
+
+def init(first) {
+    First = first
+}
+def test1(){
+    git clone https://github.com/Rajugithub1989/java-hello-world-webapp-1.git
+}
+def test2(){
+    First.test2()
+}
+return this
