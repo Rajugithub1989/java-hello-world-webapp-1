7b9c1e0ac0230c7fbb784dd1e4dd4951fc03fe36
rkbs <renukaraju.kbs@dxc.com>
2023-07-17 13:15:55 +0530

    13

--- common-pipeline.groovy
+++ common-pipeline.groovy
@@ -25,12 +25,12 @@
 // // }
 
 
-def get_project_code(repo_url){
+def firstTest(){
     project_codes = [
         "https://github.com/Rajugithub1989/java-hello-world-webapp-1.git": '**Rboss9743483266**',
         "https://github.com/Rajugithub1989/java-hello-world-webapp-2.git": '**Rboss9743483266**',
         "https://github.com/Rajugithub1989/java-hello-world-webapp-3.git": '**Rboss9743483266**'
     ]
-    return project_codes[repo_url]
+   // return project_codes[repo_url]
 }
 return this
